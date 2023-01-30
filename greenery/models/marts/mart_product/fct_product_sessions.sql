575d104ee94b00ccc29cf8f9978c8babc1a805fd
MartinaGelnerova <martina.gelner.cz@gmail.com>
2023-01-30 17:29:45 +0000

    Week 3 updates

--- greenery/models/marts/mart_product/fct_product_sessions.sql
+++ greenery/models/marts/mart_product/fct_product_sessions.sql
@@ -4,5 +4,5 @@ select --product_guid
     , count(distinct(iff(event_type = 'add_to_cart', session_guid, null))) as add_to_carts_ttl
     , count(distinct(iff(event_type = 'checkout', session_guid, null))) as checkouts_ttl
     , checkouts_ttl / views_ttl as product_conversion_rate
-from DEV_DB.DBT_MARTINAGELNEROVAAPIFYCOM.INT_SESSION_PRODUCT
+from {{ref('int_session_product')}}
 group by product_name
